f8404af77915b9923b50fb540abe95ccfe77bc8d
Adriano Dennanni <adriano.dennanni@infosimples.com.br>
2018-09-17 10:31:23 -0300

    Add csv gem

---
+++ train_pet_recon_model.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import re
+import tensorflow as tf
+
+TOTAL_EPOCHS = 30
+BATCH_SIZE = 16
+TRAIN_DATASET_PATH = './datasets/train'
+VALIDATION_DATASET_PATH = './datasets/validation'
