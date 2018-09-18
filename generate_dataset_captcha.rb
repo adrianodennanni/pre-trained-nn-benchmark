584cc41eb5da529f93adda310d0a43a099764432
Adriano Dennanni <adrianodennanni@gmail.com>
2018-09-18 14:50:30 -0300

    Changed captcha generation

--- generate_dataset_captcha.rb
+++ generate_dataset_captcha.rb
@@ -14,8 +14,8 @@ unless Dir.exist?(dir_path)
 end
 
 # Function for generate and save captchas
-def save_random_captcha(text, dataset)
-  image = Magick::Image.new(150, 50)
+def save_random_captcha(text)
+  image = Magick::Image.new(160, 80)
   image.format = "jpg"
   image.gravity = Magick::CenterGravity
   image.background_color = 'white'
@@ -23,14 +23,16 @@ def save_random_captcha(text, dataset)
   draw = Magick::Draw.new
   draw.annotate(image, image.columns, image.rows, 0, 0, text) {
     self.gravity = Magick::CenterGravity
-    self.pointsize = 32
+    self.pointsize = 28
     self.fill = 'darkblue'
     self.stroke = 'transparent'
+    self.rotation = rand(-20..20)
   }
 
-  image = image.wave(4 + rand(2), 60 + rand(15))
-  image = image.implode((1 + rand(2)) / 10.0)
-  image = image.swirl(rand(10))
+  image = image.gaussian_blur(0.0, rand)
+  image = image.implode((rand(1)) / 10.0)
+  image = image.swirl(rand(15))
+  image = image.radial_blur(rand(5))
   image = image.add_noise(Magick::ImpulseNoise)
 
   File.write("./captcha_dataset/#{dataset}/#{text}.jpg", image.to_blob)
@@ -39,12 +41,12 @@ end
 # Start saving the captcha_dataset
 texts = []
 (1..35_000).each { texts << SecureRandom.alphanumeric(6).upcase }
-Parallel.each(texts.uniq, in_processes: 30) { |text| save_random_captcha(text, 'train') }
+Parallel.each(texts.uniq, in_processes: 30, progress: "Creating train captchas") { |text| save_random_captcha(text, 'train') }
 
 texts = []
 (1..2_000).each { texts << SecureRandom.alphanumeric(6).upcase }
-Parallel.each(texts.uniq, in_processes: 30) { |text| save_random_captcha(text, 'validation') }
+Parallel.each(texts.uniq, in_processes: 30, progress: "Creating validation captchas") { |text| save_random_captcha(text, 'validation') }
 
 texts = []
 (1..2_000).each { texts << SecureRandom.alphanumeric(6).upcase }
-Parallel.each(texts.uniq, in_processes: 30) { |text| save_random_captcha(text, 'test') }
+Parallel.each(texts.uniq, in_processes: 30, progress: "Creating test captchas") { |text| save_random_captcha(text, 'test') }
